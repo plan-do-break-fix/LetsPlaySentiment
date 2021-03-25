03263e3627ebff9bf66c0f199965e26ed1215224
plan-do-break-fix <jeffdswan@outlook.com>
2021-03-25 00:27:40 -0500

    docstring

--- featuresApp/App.py
+++ featuresApp/App.py
@@ -10,6 +10,7 @@ class App:
         self.terms = self.load_search_terms()
 
     def collect_playlists(self, game_title: str) -> List[str]:
+        """Return list of ids for confirmed matching transcribed playlists.""" 
         candidates = self.db.get_candidates(game_title)
         matching = [playlist for playlist in candidates \
                     if confirmed_match(self.db.get_playlist_title(playlist), 
@@ -17,6 +18,7 @@ class App:
         return matching
 
     def load_transcript(playlist: str) -> str:
+        """Returns text of transcripts from local storage."""
         if not os.path.isfile(f"/data/transcripts/{playlist}.txt"):
             return ""
         with open(f"/data/transcripts/{playlist}.txt", "r") as _f:
